0944a91d8fde3359379a849daeb91cc9c8aafe7d
Bruh <sasapak820@gmail.com>
2025-11-24 20:56:38 +0300

    lab

--- lab5.py
+++
@@ -1,9 +0,0 @@
-def find_smallest_k(N):
-    K = 1
-    while 3 ** K <= N:
-        K += 1
-    return K
-
-N = int(input("Введите число N: "))
-K = find_smallest_k(N)
-print(K)
--- lab52.py
+++
@@ -1,5 +0,0 @@
-m = 80
-m1 = 400
-for m2 in range(300, 0, -20):
- A = m2 - m1 / m1 + m2 + m/2 * 9.8
- print(A)
