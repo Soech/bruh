1e2722567c67980e0a4ac8f279ee866a86c3b2c0
Soech <sasapak820@gmail.com>
2026-03-11 00:43:40 +0300

    lab 3

--- asdads/Untitled-1.cpp
+++ asdads/Untitled-1.cpp
@@ -6,25 +6,38 @@ float zadanie1(float a, float z)
 {
     float znamenatel;
     znamenatel = pow(a, 2) + tan(z);
+    if (znamenatel > 0) 
+    {
     float zcubic, cos_z, result;
     zcubic = pow(z, 3);
     cos_z = cos(zcubic);
     result = pow(cos_z, 2) - z / sqrt(znamenatel);
     return result;
+    }
+    else
+    {
+        return NAN;
+    }
 }
-
 
 int zadanie2(float x)
 {
     if (x < 1) 
     {
-        return cbrt(5 * pow(x, 2) + 9 * x) + 3 * x;
-    } 
+        float cubicroot;
+        cubicroot = cbrt(5 * pow(x, 2) + 9 * x) + 3 * x;
+        if (cubicroot < 0);
+        {
+            return NAN;
+        }
+        return cubicroot;
+    }
     else if (x >= 1 && x < 4)
     {
-        float znamenatel;
-        znamenatel = 2 * pow(x, 3) + exp(-x);
-        return (pow(x, 5) - 2 * pow(x, 4) - 5) / znamenatel;
+        float znamenatel = 2 * pow(x, 3) + exp(-x);
+        float chislitel = pow(x, 5) - 2*pow(x, 4) -5;
+        float result = chislitel * znamenatel;
+        return result;
     }
         else
     {
@@ -34,6 +47,7 @@ int zadanie2(float x)
 
 int main()
 {
+    setlocale(LC_ALL, "Rus");
     float a, z;
     cout << "Input a: ";
     cin >> a;
@@ -42,10 +56,8 @@ int main()
     float result = zadanie1(a, z);
     cout << "Result 1: " << result << endl;
     float x, y, result1;
-    cout << "input: x";
+    cout << "input x: ";
     cin >> x;
-    cout << "input: y";
-    cin >> y;
     result1 = zadanie2(x);
     cout << "Result 2: " << result1 << endl;
 }
